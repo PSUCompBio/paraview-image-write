e9ec24915cbfe431e470645dacd08ab83798f3f8
Reuben Kraft <reuben.kraft@gmail.com>
2019-09-21 09:05:31 -0400

    Update pass_check.sh

--- pass_check.sh
+++ pass_check.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Add file and the target file size below
+repo=paraview-image-write
 file=test.png
 target_file_size=332235
 
@@ -12,6 +13,9 @@ echo Target File Size = "$target_file_size"
 
 if [ $myfilesize -ge $target_file_size ];then
         echo Passed!
+        echo "Passed" >> ~/$repo.PASSED
+        echo "Acutal File Size = "$myfilesize" " >> ~/$repo.PASSED
+        echo "Target File Size = "$target_file_size" " >> ~/$repo.PASSED
 else
         echo Failed!
 fi
